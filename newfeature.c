18e55f3316a98c4dd62425dc2cf14758f2d7fa75
yeobin <yeobin-geek@gmail.com>
2025-11-12 17:11:33 +0900

    Resove conflict

---
+++ newfeature.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void main()
+{
+	// need to print current working dir.
+	char pwd[1024];
+	FILE *f = fopen("pwd-log.log", 'w');
+	getcwd(pwd,sizeof(pwd));
+	printf("print working dir: %s\n", pwd);
+	fputs("%s\n", f);
+	fclose(f);
+}
